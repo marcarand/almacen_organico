d227d6410967205da47a0d2aaad7111e870b8de1
marcarand <marcarand@riseup.net>
2021-10-17 20:46:13 -0300

    changuito

--- src/App.js
+++ src/App.js
@@ -1,30 +1,43 @@
-import Encabezado from "./componentes/Encabezado.jsx";
-import Pie from "./componentes/Pie.jsx";
+import Encabezado from "./componentes/Encabezado";
+import Pie from "./componentes/Pie";
 import {useState} from "react";
-import Producto from "./componentes/Producto.jsx";
-/*
-    revisar la importación
-*/
-
+import Producto from "./componentes/Producto";
+import { Fragment } from 'react';
+import Changuito from "./componentes/Changuito";
 
 function App() {
 
-const [productos, guardarProductos] = useState([
-    {id:1, nombre:"Yerba Mate", precio:550},
-    {id:2, nombre:"Tomate Triturado", precio:420},
-    {id:3, nombre:"Lentejas x kilo", precio:349},
-    {id:4, nombre:"Arroz Integral x kilo", precio:525},
+const [productos] = useState([
+    {id:1, articulo:"Yerba Mate", precio:550},
+    {id:2, articulo:"Tomate Triturado", precio:420},
+    {id:3, articulo:"Lentejas x kilo", precio:349},
+    {id:4, articulo:"Arroz Integral x kilo", precio:525},
 ]);
 
+const [changuito, agregarProducto, eliminarProducto] = useState([]);
+
   return (
-    <fragment>
+    <Fragment>
         <Encabezado />
+        <Changuito
+            productos={productos}
+            changuito={changuito}
+            agregarProducto={agregarProducto}
+            eliminarProducto={eliminarProducto}
+        />
         Almacén Orgánico - compra online
         {productos.map( producto =>
-        <Producto producto={producto} />
+        <Producto
+            key={ Date.now() + producto.id }
+            productos={productos}
+            producto={producto}
+            changuito={changuito}
+            agregarProducto={agregarProducto}
+            eliminarProducto={eliminarProducto}
+        />
         )}
         <Pie fecha={2021}/>
-    </fragment>
+    </Fragment>
   );
 }
 
--- src/componentes/Producto.jsx
+++ src/componentes/Producto.jsx
@@ -1,10 +1,42 @@
 import React from "react";
+import { Fragment } from "react";
 
-const Producto = ({producto}) => {
+const Producto = ({producto, productos, changuito, agregarProducto}) => {
+    const {id, articulo, precio} = producto;
+    const seleccionarProducto = ({id}) => {
+        let producto = productos.filter(
+            producto => producto.id === id
+        )[0];
+        let nuevo_producto = Object.assign({}, producto);
+        nuevo_producto.id = Date.now()
+        agregarProducto([...changuito, nuevo_producto]);
+    }
+    const eliminarProducto = ({id}) => {
+        const nuevo_changuito = changuito.filter(
+            producto => producto.id !== id
+        );
+        agregarProducto(nuevo_changuito)
+    }
+
     return (
-        <div>
-        {producto.id} | {producto.nombre} | {producto.precio}
-        </div>
+        <Fragment>
+            <div>
+               {id} | {articulo} | $ {precio}
+                {
+                    productos
+                    ?
+                        <button
+                            type="button"
+                            onClick={()=>seleccionarProducto({id})}
+                        >Comprar</button>
+                    :
+                        <button
+                            type="button"
+                            onClick={()=>eliminarProducto({id})}
+                        >Eliminar</button>
+                }
+            </div>
+        </Fragment>
     );
 }
 
